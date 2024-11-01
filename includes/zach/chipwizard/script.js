56ce3310663d5aaa1edb504bf0c54197a6c625d3
MarshDeer <marshdeer97@gmail.com>
2024-10-31 23:49:58 -0300

    first version of chipwizard 3d chip animation

--- includes/zach/chipwizard/script.js
+++ includes/zach/chipwizard/script.js
@@ -38,9 +38,23 @@ function init() {
 
 function create(target) {
 	const names = ['one', 'two', 'three', 'four', 'five', 'six'];
+	const percent = [1.6, 3.2, 4.8, 6.4, 8, 9.6, 11.2, 12.8, 14.4, 16, 17.6, 19.2, 20.8, 22.4, 24, 25.6, 27.2, 28.8, 30.4, 32, 33.6, 35.2, 36.8, 38.4, 40, 41.6, 43.2, 44.8, 46.4, 48, 49.6, 51.2, 52.8, 54.4, 56, 57.6, 59.2, 60.8, 62.4, 64, 65.6, 67.2, 68.8, 70.4, 72, 73.6, 75.2, 76.8, 78.4, 80, 81.6, 83.2, 84.8, 86.4, 88, 89.6, 91.2, 92.8, 94.4];
 	console.log(levelData[target].title);
 	// chip preview animation
 	document.getElementById('preview').removeAttribute('hidden');
+	let visualizer = document.querySelector('.visualizer');
+	visualizer.querySelector('style').remove();
+	let keyframes = '';
+	for (let legIteration = 0; legIteration < 6; legIteration++) {
+		keyframes = keyframes + `@keyframes ${names[legIteration]} {\n`;
+		for (let tickIteration = 0; tickIteration < 59; tickIteration++) {
+			keyframes = keyframes + `${percent[tickIteration]}% {background: var(--${levelData[target].signalValue[legIteration][tickIteration]});}\n`;
+		}
+		keyframes = keyframes + `100% {background: var(--${levelData[target].signalValue[legIteration][58]});}\n}\n`;
+	}
+	let animations = document.createElement('style');
+	animations.innerHTML = keyframes;
+	visualizer.append(animations);
 
 	// layout src
 	document.getElementById('layout').removeAttribute('hidden')
